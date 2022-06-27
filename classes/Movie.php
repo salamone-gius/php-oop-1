3c372f386793b05defc53acf4d83cf78eeb62e62
salamone-gius <salamone.gius@gmail.com>
2022-06-27 17:55:19 +0200

    comments added

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,13 +1,16 @@
 <?php
 
+// creo la classe Movie... 
 class Movie {
+    // ...con le proprietà che ogni oggetto dovrà avere
     public $title;
     public $genre;
+    // setto $year come 'private' per non condividere il suo valore
     private $year;
     public $direction;
     public $mainCast;
 
-
+    // creo la funzione che convalida l'anno
     private function yearValidation($_year) {
         if ($_year > 1900) {
             return true;
@@ -16,16 +19,19 @@ class Movie {
         }
     }
 
+    // creo la setter function per SETTARE (SCRIVERE) l'anno con la funzione di validazione all'interno
     public function setYear($_year) {
         if ($this->yearValidation($_year)) {
             $this->year = $_year;
         }
     }
 
+    // creo la getter function per OTTENERE (LEGGERE) l'anno
     public function getYear() {
         return $this->year;
     }
 
+    // creo il costruttore che genererà gli oggetti appartenenti alla classe Movie
     public function __construct($_title, $_genre, $_year)
     {
         $this->title = $_title;
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 
+    // collego il file contenente la classe Movie
     require_once __DIR__ . '/classes/Movie.php';
 
+    // creo un array...
     $movies = [
+        // ...che popolo con l'istanza di 3 nuovi oggetti anonimi
         new Movie('Kingsman: The Secret Service', 'spy-fiction', 2014),
         new Movie('Murder on the Orient Express', 'crime-thriller', 2017),
         new Movie('Uncharted', 'adventure', 2022),
@@ -23,10 +26,12 @@
     <body>
         <h1 style="text-align:center;">Movies</h1>
         <ul>
+            <!-- ciclo l'array $movies per stampare i miei oggetti -->
             <?php foreach ($movies as $movie) { ?>
                 <li>
                     <h2 style="margin-top: 40px;"><?php echo $movie->title; ?></h2>
                     <p>Genre: <?php echo $movie->genre; ?></p>
+                    <!-- per LEGGERE e stampare la proprietà year (PRIVATE) devo usare la funzione getter -->
                     <p>Year: <?php echo $movie->getYear(); ?></p>
                 </li>
             <?php }; ?>
