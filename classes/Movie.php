cfbb8c6d2c88115e274bd47e6ea319a5048db6a3
salamone-gius <salamone.gius@gmail.com>
2022-06-27 16:49:21 +0200

    class Movie created and linked on index.php

---
+++ classes/Movie.php
@@ -0,0 +1,11 @@
+<?php
+
+class Movie {
+    public $title;
+    public $genre;
+    public $year;
+    public $direction;
+    public $mainCast;
+}
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+
+    require_once __DIR__ . '/classes/Movie.php';
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
